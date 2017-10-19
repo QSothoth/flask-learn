872eca6e0c786feecedbd5c491ab1376c4e55dea
Sothoth <RorschachShi@outlook.com>
2017-10-19 20:33:25 +0800

    learn flask-nav

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 # hello word
 from flask import Blueprint, render_template
 from .nav import nav
-from flask_nav.elements import *
+from flask_nav.elements import Navbar, View, Separator, Subgroup, Text, Link
 
 main = Blueprint('main', __name__)
 
@@ -10,7 +10,18 @@ main = Blueprint('main', __name__)
 nav.register_element('index_top', Navbar(
     u'Rorschach',
     View('Blog', 'main.blog'),
-    View('About', 'main.about'),))
+    View('About', 'main.about'),
+    Subgroup(
+        'Products',
+        View('Blog', 'main.blog'),
+        View('About', 'main.about'),
+        Separator(),
+        Text('Discontinued Products'),
+        View('Blog', 'main.blog'),
+        View('About', 'main.about'),
+        Link('Tech Support', 'http://techsupport.invalid/widgits_inc'),
+    )
+))
 print nav
 
 @main.route('/')
