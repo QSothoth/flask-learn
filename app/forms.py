8c957dc2165b83023b15320b0ee4f0dc2b706c1d
Sothoth <RorschachShi@outlook.com>
2017-10-26 18:02:40 +0800

    learn flask-wtf

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired, Length
 
 
 class LoginForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired(message=u'用户名不能为空'),
+                                           Length(2, 20, message=u'用户名长度不对')],
+                       render_kw={"placeholder": "Your name"})
+    password = StringField('password', validators=[DataRequired()],
+                           render_kw={"placeholder": "password"})
+    remember_me = BooleanField('remember me', default=False)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # hello word
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect , url_for
 from .nav import nav
-from flask_nav.elements import Navbar, View, Separator, Subgroup, Text, Link
+from flask_nav.elements import Navbar, View, Separator, Subgroup, Link
 from .forms import LoginForm
 
 main = Blueprint('main', __name__)
@@ -14,23 +14,27 @@ nav.register_element('index_top', Navbar(
     View('About', 'main.about'),
     Subgroup(
         'Products',
-        View('Blog', 'main.blog'),
+        View('login', 'main.login'),
         View('About', 'main.about'),
+        Separator(),
         Link('Tech Support', 'http://techsupport.invalid/widgits_inc'),
-    )
+    ),
+    View('About', 'main.about'),
 ))
-print nav
+
 
 @main.route('/')
 def index():
     return render_template('index.html', title='Personal Site')
 
+
 @main.route('/login', methods=('GET', 'POST'))
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return 'ok'
+        return redirect(url_for('main.index'))
     return render_template('login.html', form=form)
+
 
 @main.route('/blog')
 def blog():
